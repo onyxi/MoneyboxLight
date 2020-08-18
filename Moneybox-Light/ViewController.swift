abd8223af865ed2f47c4711eb08919ab2344155e
Pete Holdsworth <pete.holdsworth@futureplatforms.com>
2020-08-18 21:50:52 +0100

    Initial Commit

---
+++ Moneybox-Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moneybox-Light
+//
+//  Created by Pete Holdsworth on 18/08/2020.
+//  Copyright © 2020 Pete Holdsworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
